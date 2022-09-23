979ae6df19cc4aa8a262e9bfadeb70e8d2e8d425
Vladimir Rusnak <rusvova@yandex.ru>
2022-09-23 20:21:39 +0500

    Перевод в long

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
 
     public static void main(String[] args) {
-        int[] sales = {100, 200, 300};
+        long [] sales = {100, 200, 300};
         SalesManager salesManager = new SalesManager(sales);
-        int max = salesManager.max();
+        long max = salesManager.max();
         System.out.println(max);
     }
 
